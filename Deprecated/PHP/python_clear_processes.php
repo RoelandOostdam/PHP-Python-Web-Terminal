022ab7ada81781fc182ea682b5d4bc85b345d6e4
Roeland Oostdam <Roeland.Oostdam@outlook.com>
2018-04-08 01:33:51 +0200

    Removed deprecated version

--- Deprecated/PHP/python_clear_processes.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'conn.php';
-  $sth = $conn->prepare("TRUNCATE python_data;");
-  $sth->execute();
-
-  header('location: python_commander.php');
-
-?>
--- Deprecated/PHP/update_response_time.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include "conn.php";
-	$sql = "UPDATE python_config SET response_time=" . $_POST['level'];
-	$conn->query($sql);
-?>
